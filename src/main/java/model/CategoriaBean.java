a4c0351baebe2d534d5ee13d615889faf52541fc
RedMulaCrackIn <giony2003@gmail.com>
2024-05-27 14:46:45 +0200

    Creazione categoria bean

--- src/main/java/model/CategoriaBean.java
+++ src/main/java/model/CategoriaBean.java
@@ -4,5 +4,22 @@ import java.io.Serializable;
 
 public class CategoriaBean implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+	private String nome;
+
+	    public CategoriaBean() {
+	        this.nome = "null";
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public static long getSerialversionuid() {
+	        return serialVersionUID;
+	    }
 }
+

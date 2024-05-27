51ad26bf83fcb0be2b0511754094703a5587f54a
RedMulaCrackIn <giony2003@gmail.com>
2024-05-27 14:57:15 +0200

    Correzione ed aggiustamenti al codice

--- src/main/java/model/OrdineBean.java
+++ src/main/java/model/OrdineBean.java
@@ -2,53 +2,53 @@ package model;
 
 import java.io.Serializable;
 
-public class OrdineBean implements Serializable{
-	private static final long serialVersionUID = 1L;
-	private int id;
-	    private String data;
-	    private double costoTotale;
-	    private String utenteUsername;
+public class OrdineBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String data;
+    private double costoTotale;
+    private String utenteUsername;
 
-	    public OrdineBean() {
-	        this.id = -1;
-	        this.data = "null";
-	        this.costoTotale = -1;
-	        this.utenteUsername = "null";
-	    }
+    public OrdineBean() {
+        this.id = -1;
+        this.data = "null";
+        this.costoTotale = -1;
+        this.utenteUsername = "null";
+    }
 
-	    public int getId() {
-	        return id;
-	    }
+    public int getId() {
+        return id;
+    }
 
-	    public void setId(int id) {
-	        this.id = id;
-	    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	    public String getData() {
-	        return data;
-	    }
+    public String getData() {
+        return data;
+    }
 
-	    public void setData(String data) {
-	        this.data = data;
-	    }
+    public void setData(String data) {
+        this.data = data;
+    }
 
-	    public double getCostoTotale() {
-	        return costoTotale;
-	    }
+    public double getCostoTotale() {
+        return costoTotale;
+    }
 
-	    public void setCostoTotale(double costoTotale) {
-	        this.costoTotale = costoTotale;
-	    }
+    public void setCostoTotale(double costoTotale) {
+        this.costoTotale = costoTotale;
+    }
 
-	    public String getUtenteUsername() {
-	        return utenteUsername;
-	    }
+    public String getUtenteUsername() {
+        return utenteUsername;
+    }
 
-	    public void setUtenteUsername(String utenteUsername) {
-	        this.utenteUsername = utenteUsername;
-	    }
+    public void setUtenteUsername(String utenteUsername) {
+        this.utenteUsername = utenteUsername;
+    }
 
-	    public static long getSerialversionuid() {
-	        return serialVersionUID;
-	    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 }
--- src/main/java/model/ProdottoBean.java
+++ src/main/java/model/ProdottoBean.java
@@ -2,93 +2,93 @@ package model;
 
 import java.io.Serializable;
 
-public class ProdottoBean implements Serializable{
-	private static final long serialVersionUID = 1L;
-	private int id;
-	    private String nome;
-	    private String descrizione;
-	    private int quantita;
-	    private int costo;
-	    private String sesso;
-	    private String immagine;
-	    private String categoriaImmagine;
+public class ProdottoBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String nome;
+    private String descrizione;
+    private int quantita;
+    private int costo;
+    private String sesso;
+    private String immagine;
+    private String categoriaImmagine;
 
-	    public ProdottoBean() {
-	        this.id = -1;
-	        this.nome = "null";
-	        this.descrizione = "null";
-	        this.quantita = -1;
-	        this.costo = -1;
-	        this.sesso = "null";
-	        this.immagine = "null";
-	        this.categoriaImmagine = "null";
-	    }
+    public ProdottoBean() {
+        this.id = -1;
+        this.nome = "null";
+        this.descrizione = "null";
+        this.quantita = -1;
+        this.costo = -1;
+        this.sesso = "null";
+        this.immagine = "null";
+        this.categoriaImmagine = "null";
+    }
 
-	    public int getId() {
-	        return id;
-	    }
+    public int getId() {
+        return id;
+    }
 
-	    public void setId(int id) {
-	        this.id = id;
-	    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	    public String getNome() {
-	        return nome;
-	    }
+    public String getNome() {
+        return nome;
+    }
 
-	    public void setNome(String nome) {
-	        this.nome = nome;
-	    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	    public String getDescrizione() {
-	        return descrizione;
-	    }
+    public String getDescrizione() {
+        return descrizione;
+    }
 
-	    public void setDescrizione(String descrizione) {
-	        this.descrizione = descrizione;
-	    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
 
-	    public int getQuantita() {
-	        return quantita;
-	    }
+    public int getQuantita() {
+        return quantita;
+    }
 
-	    public void setQuantita(int quantita) {
-	        this.quantita = quantita;
-	    }
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
 
-	    public int getCosto() {
-	        return costo;
-	    }
+    public int getCosto() {
+        return costo;
+    }
 
-	    public void setCosto(int costo) {
-	        this.costo = costo;
-	    }
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
 
-	    public String getSesso() {
-	        return sesso;
-	    }
+    public String getSesso() {
+        return sesso;
+    }
 
-	    public void setSesso(String sesso) {
-	        this.sesso = sesso;
-	    }
+    public void setSesso(String sesso) {
+        this.sesso = sesso;
+    }
 
-	    public String getImmagine() {
-	        return immagine;
-	    }
+    public String getImmagine() {
+        return immagine;
+    }
 
-	    public void setImmagine(String immagine) {
-	        this.immagine = immagine;
-	    }
+    public void setImmagine(String immagine) {
+        this.immagine = immagine;
+    }
 
-	    public String getCategoriaImmagine() {
-	        return categoriaImmagine;
-	    }
+    public String getCategoriaImmagine() {
+        return categoriaImmagine;
+    }
 
-	    public void setCategoriaImmagine(String categoriaImmagine) {
-	        this.categoriaImmagine = categoriaImmagine;
-	    }
+    public void setCategoriaImmagine(String categoriaImmagine) {
+        this.categoriaImmagine = categoriaImmagine;
+    }
 
-	    public static long getSerialversionuid() {
-	        return serialVersionUID;
-	    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 }

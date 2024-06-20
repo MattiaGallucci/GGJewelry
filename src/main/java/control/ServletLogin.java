49553ed6e4145a43e568c033f909a3a50d3c0ed6
RedMulaCrackIn <giony2003@gmail.com>
2024-06-20 18:02:55 +0200

    Servlet Login completed

--- src/main/java/control/ServletLogin.java
+++ src/main/java/control/ServletLogin.java
@@ -1,33 +1,70 @@
 package control;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLException;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.UtenteBean;
+import model.UtenteDAO;
 
-@WebServlet("/ServletLogin")
+@WebServlet("/login")
+
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public ServletLogin() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String utente = (String) request.getSession().getAttribute("utente");
-		boolean admin = (boolean) request.getSession().getAttribute("admin");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		Encoder encoder = Base64.getEncoder();
+		String password64 = encoder.encodeToString(request.getParameter("password").getBytes());
+		UtenteBean user = checkLogin(username, password64);
+
+		if (user != null) {
+			request.getSession().setAttribute("nome", user.getNome());
+			request.getSession().setAttribute("cognome", user.getCognome());
+			request.getSession().setAttribute("utente", user.getUsername());
+			request.getSession().setAttribute("logged", true);
+
+			if (user.isAdmin()) {
+				request.getSession().setAttribute("admin", true);
+			} else {
+				request.getSession().setAttribute("admin", false);
+			}
+
+			response.sendRedirect("index.jsp");
+		} else {
+			request.getSession().setAttribute("logged", false);
+			request.getSession().setAttribute("error", "Username e/o password invalidi.");
+			response.sendRedirect("loginForm.jsp?action=error");
+		}
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	private UtenteBean checkLogin(String username, String password64) 
+	{
+		UtenteDAO database = new UtenteDAO();
+		UtenteBean user = new UtenteBean();
+		
+		try 
+		{
+			user = database.doRetrieveByUsername(username);
+			
+			if(user.getUsername().equals(username) && user.getPassword().equals(password64)) {
+				return user;
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
-
 }

f2c708b92e6693e5ce7378407936a8885f774087
RedMulaCrackIn <giony2003@gmail.com>
2024-06-25 18:44:07 +0200

    fixed catalogo servlet

--- src/main/java/control/CatalogoServlet.java
+++ src/main/java/control/CatalogoServlet.java
@@ -1,6 +1,8 @@
 package control;
+
 import model.ProdottoBean;
 import model.ProdottoDAO;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,115 +14,68 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-@WebServlet("/catalogo")
 
-public class CatalogoServlet extends HttpServlet 
-{
+@WebServlet("/catalogo")
+public class CatalogoServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    
+
     public CatalogoServlet() {
         super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ProdottoDAO dao = new ProdottoDAO();
-<<<<<<< HEAD
-        ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
+        List<ProdottoBean> prodotti = new ArrayList<>();
 
         try {
-            ArrayList<ProdottoBean> abbigliamento = dao.doRetrieveByCategoria("Abbigliamento");
-            ArrayList<ProdottoBean> anelli = dao.doRetrieveByCategoria("anelli");
-            ArrayList<ProdottoBean> collane = dao.doRetrieveByCategoria("collane");
+            // Retrieve all products
+            prodotti = dao.doRetrieveAll("");
 
-            categorie.add(abbigliamento);
-            categorie.add(anelli);
-            categorie.add(collane);
+            // Set products list in session
+            request.getSession().setAttribute("prodotti", prodotti);
 
-            request.getSession().setAttribute("categorie", categorie);
+            // Get 3 random products
+            List<ProdottoBean> randomProdotti = getRandomProducts(prodotti, 3);
 
-            // Combine all products into a single list for random selection
-            List<ProdottoBean> allProducts = new ArrayList<>();
-            allProducts.addAll(abbigliamento);
-            allProducts.addAll(anelli);
-            allProducts.addAll(collane);
+            // Set random products list in request attribute
+            request.setAttribute("randomProdotti", randomProdotti);
 
-            // Select random products
-            List<ProdottoBean> randomProducts = getRandomProducts(allProducts, 10); // for example, select 10 random products
-            request.getSession().setAttribute("randomProducts", randomProducts);
-
-            // Log image URLs for debugging
-            for (ProdottoBean product : randomProducts) {
-                System.out.println("Product Image URL: assets/images/" + product.getImmagine());
-            }
-
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
+        // Forward to the homePage.jsp
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/homePage.jsp");
         dispatcher.forward(request, response);
-    }
-
-    private List<ProdottoBean> getRandomProducts(List<ProdottoBean> allProducts, int numberOfProducts) {
-        List<ProdottoBean> randomProducts = new ArrayList<>();
-        Random rand = new Random();
-        for (int i = 0; i < numberOfProducts && !allProducts.isEmpty(); i++) {
-            int randomIndex = rand.nextInt(allProducts.size());
-            randomProducts.add(allProducts.remove(randomIndex));
-        }
-        return randomProducts;
-=======
-        List<ProdottoBean> prodotti = new ArrayList<>();
-		
-        try {
-            // Retrieve all products
-			prodotti = dao.doRetrieveAll("");
-			
-			// Set products list in session
-			request.getSession().setAttribute("prodotti", prodotti);
-			
-			// Get 3 random products
-			List<ProdottoBean> randomProdotti = getRandomProducts(prodotti, 3);
-			
-			// Set random products list in session
-			request.getSession().setAttribute("randomProdotti", randomProdotti);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		// Forward to the homePage.jsp
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/homePage.jsp");
-		dispatcher.forward(request, response);
->>>>>>> branch 'master' of https://github.com/MattiaGallucci/GGJewelry.git
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
-    
+
     /**
      * Retrieves a list of random products from the given list.
-     * 
+     *
      * @param products List of all products available
-     * @param count Number of random products to retrieve
+     * @param count    Number of random products to retrieve
      * @return List of randomly selected products
      */
     private List<ProdottoBean> getRandomProducts(List<ProdottoBean> products, int count) {
         List<ProdottoBean> randomProducts = new ArrayList<>();
         Random random = new Random();
-        
+
         // Ensure we don't exceed available products count
         int maxIndex = Math.min(count, products.size());
-        
+
         // Select random products
         for (int i = 0; i < maxIndex; i++) {
             int randomIndex = random.nextInt(products.size());
             ProdottoBean randomProduct = products.get(randomIndex);
             randomProducts.add(randomProduct);
+            // Remove selected product to avoid duplication
+            products.remove(randomIndex);
         }
-        
+
         return randomProducts;
     }
 }
-

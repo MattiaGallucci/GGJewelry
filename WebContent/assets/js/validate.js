137097071821340f50fa3cf7a5c2f9158b9bbc86
RedMulaCrackIn <giony2003@gmail.com>
2024-06-25 11:59:53 +0200

    Adding ajax control form

--- WebContent/assets/js/validate.js
+++ WebContent/assets/js/validate.js
@@ -1,286 +1,136 @@
-$(document).ready(function(){
-	
-    let emailValidator = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    let usernameValidator = /^[a-zA-Z0-9_.]{4,15}$/;
-    let nameValidator = /^[a-zA-Z]{3,}$/;
-    let validEmail = false;
-    let validUsername = false;
-    let validPwd = false;
-    let validPwdCheck = false;
-    let validName = false;
-    let validSurname = false;
-    
-    $("#email").keyup(function() {
+const textAreaRegEx = /^[a-zA-Z0-9\s,\.!?éçòàùéè^()&$£€';":^]+$/;
+const textRegEx = /^[a-zA-Z0-9\s'àèòùìéçé]+$/;
+const emailRegEx = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
+const passwordRegEx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9#?!@$%^&*-]{8,}$/;
 
-        let email = $("#email").val();
-        let slideEmail = 0;
-        
-        if (email.match(emailValidator)) {
+function showError(input, message) {
+	input.style.border = "2px solid red";
+	const errorElement = document.getElementById(input.id + "-error");
+	errorElement.textContent = message;
+}
 
-            console.log("email valida");
-            $.ajax({
-                type: "POST",
-                url: "login",
-                data: {
-                	mode: "checkEmail",
-                    email: email,
-                },
-                dataType: "html",
-                success: function(data) {
-                	console.log(data);
+function clearError(input) {
+	input.style.border = "none";
+	const errorElement = document.getElementById(input.id + "-error");
+	errorElement.textContent = "";
+}
 
-                    if(data.match("non disponibile")) {
-                    	
-                    	validEmail = false;
-                        if(slideEmail == 0){
-                            $("#error-email").slideDown();
-                            slideEmail = 1;
-                        }
+function passwordMatchValidate(password, passwordR) {
+	var m = document.getElementById("errorMessage");
+	if (password != passwordR) {
+		m.style.color = "red";
+		showError(document.getElementById("passwordR"), "Le due password non corrispondono!");
+		return false;
+	}
+	m.innerHTML = "";
+	clearError(document.getElementById("passwordR"));
+	return true;
+}
 
-                    	$("#error-email").text("Email gia in uso");
+function passwordValidate(input) {
+	const passwordResult = passwordRegEx.test(input.value);
 
-                    } else if (data.match("disponibile")){
-                        slideEmail = 0;
-                        validEmail = true;
-                        $("#error-email").slideUp();
-                    	$("#error-email").text("");
-                    }
-                }
-            });
+	if (!passwordResult) {
+		showError(input, "Password non valida! La password deve essere di almeno 8 caratteri, deve contenere un numero, una lettera maiuscola, una lettera minuscola e NESSUN carattere speciale.");
+		input.focus();
+		return false;
+	}
+	clearError(input);
+	return true;
+}
 
-        } else {
+function textValidate(input) {
+	const textResult = textRegEx.test(input.value);
 
-            console.log("email non valida");
-            validEmail = false;
-            if(slideEmail == 0){
+	if (!textResult) {
+		showError(input, "Campo vuoto o Formato errato caratteri consentiti: a-z A-Z 0-9");
+		input.focus();
+		return false;
+	}
+	clearError(input);
+	return true;
+}
+function textAreaValidate(input) {
+	const textResult = textAreaRegEx.test(input.value);
 
-                $("#error-email").slideDown();
-                slideEmail = 1;
+	if (!textResult) {
+		showError(input, "Campo vuoto o Formato errato sono consentite solo a-z A-Z 0-9  e i simboli ,?.:!;^()&$£€\"");
+		input.focus();
+		return false;
+	}
+	clearError(input);
+	return true;
+}
+function capValidate(input) {
+	const capRegEx = /^[0-9]{5}$/;
+	const capResult = capRegEx.test(input.value);
 
-            }
+	if (!capResult) {
+		showError(input, "Campo vuoto o CAP non valido!");
+		input.focus();
+		return false;
+	}
+	clearError(input);
+	return true;
+}
 
-            $("#error-email").text("Inserisci un email valida");
+function emailValidate(input) {
+	const emailResult = emailRegEx.test(input.value);
 
-        }
+	if (!emailResult) {
+		showError(input, "Campo vuoto o Email non valida!");
+		input.focus();
+		return false;
+	}
+	clearError(input);
+	return true;
+}
+function prezzoValidate(input) {
+	const n = input.value;
+	if(n<=0){
+		showError(input, "Inserire un numero >= 0");
+		input.focus();
+		return false;
+	}
+	clearError(input);
+		return true;
+}
+function imgValidate(input) {
+	const file = input.files[0];
+	const validTypes = ['image/png', 'image/gif', 'image/jpeg'];
+	if (file && !validTypes.includes(file.type)) {
+		alert('Errore, formati accettati: .png .gif .jpeg');
+		input.value = '';
+	}
+}
+function validate() {
+	const nome = document.getElementById("name");
+	const cognome = document.getElementById("surname");
+	const via = document.getElementById("via");
+	const cap = document.getElementById("cap");
+	const paese = document.getElementById("city");
+	const email = document.getElementById("email");
+	const password = document.getElementById("password");
+	const passwordR = document.getElementById("passwordR");
+	const prezzo = document.getElementById("prezzo");
+	const descrizione = document.getElementById("descrizione");
+	const img = document.getElementById("file");
+	return textValidate(nome) &&
+		textValidate(cognome) &&
+		textValidate(via) &&
+		capValidate(cap) &&
+		textValidate(paese) &&
+		emailValidate(email) &&
+		passwordValidate(password) &&
+		passwordMatchValidate(password.value, passwordR.value) &&
+		imgValidate(img)&&
+		prezzoValidate(prezzo);
+}
 
-        if(email == "") {
-        	validEmail = false;
-            if(slideEmail == 1) {
-            	
-                slideEmail = 0;
-                $("#error-email").slideUp();
+document.getElementById("RegForm").addEventListener("submit", function(event) {
+	if (!validate()) {
+		event.preventDefault();
+	}
+}
+);
 
-            }
-
-            $("#error-email").text("");
-
-        }
-    });
-    
-    $("#username").keyup(function (){
-
-    	let username = $("#username").val();
-        let slideUsername = 0;
-
-        if(username.match(usernameValidator)){
-
-    		console.log("username formattato bene");
-    		
-    		$.ajax({
-                type: "POST",
-                url: "login",
-                data: {
-                	mode: "checkUsername",
-                    username: username,
-                },
-                dataType: "html",
-                success: function(data) {
-                	console.log(data);
-                    if(data.match("non disponibile")) {
-                    	validUsername = false;
-                        if(slideUsername == 0) {
-
-                            $("#error-username").slideDown();
-                            slideUsername = 1;
-
-                        }
-
-                    	$("#error-username").text("Username non disponibile e/o gia in uso");
-
-                    } else if (data.match("disponibile")){
-                    	validUsername = true;
-                        slideUsername = 0;
-                        $("#error-username").text("Username disponibile");
-                        $("#error-username").slideUp();
-
-                    }
-                }
-            });
-
-    	} else {
-
-            console.log("username non disponibile");
-            validUsername = false;
-            if(slideUsername == 0) {
-
-                $("#error-username").slideDown();
-                slideUsername = 1;
-
-            }
-
-    		$("#error-username").text("Username non valido");
-
-    	}
-
-        if(username == ""){
-        	validUsername = false;
-            if(slideUsername == 1) {
-                
-                slideUsername = 0;
-                $("#error-username").slideUp();
-        
-            }
-
-            $("#error-username").text("");
-
-        }
-    });
-    
-    $("#password").keyup(function (){
-    	let pwd = $("#password").val();
-    	slidePwd = 0;
-    	if(pwd.length < 5){
-    		validPwd = false;
-    		if(slidePwd == 0){
-    			$("#error-pwd").text("Password troppo corta!");
-    			$("#error-pwd").slideDown();
-    			slidePwd = 1;
-    		} else {
-    			$("#error-pwd").text("Password troppo corta!");
-    		}
-    	} else {
-    		validPwd = true;
-    		
-    		if(slidePwd == 1){
-    			$("#error-pwd").slideUp();
-    			slidePwd = 0;
-    		}
-    		
-    		$("#error-pwd").text("");
-    	}
-    	
-    	
-    });
-    
-    $("#passwordCheck").keyup(function () {
-
-        let slidePwd = 0;
-        let pwd = $("#password").val();
-        let pwdCheck = $("#passwordCheck").val();
-        
-        if(pwdCheck.valueOf() === pwd.valueOf()){
-        	
-            console.log("Le password corrispondono!");
-            slidePwd = 0;
-            validPwdCheck = true;
-            $("#error-pwdchk").slideUp();
-            $("#error-pwdchk").text("");
-
-        } else {
-
-            console.log("Le password non corrispondono");
-            validPwdCheck = false;
-            if(slidePwd == 0) {
-
-                $("#error-pwdchk").slideDown();
-                slidePwd = 1;
-
-            }
-
-            $("#error-pwdchk").text("Le password non corrispondono");
-
-        }
-    });
-    
-    $("#nome").keyup(function () {
-    	let slideNome = 0;
-    	let nome = $("#nome").val();
-    	
-    	if(nome.match(nameValidator)){
-    		console.log("Nome valido");
-    		validName = true;
-    		
-    		if(slideNome == 1){
-    			slideNome = 0;
-    			$("#error-name").slideUp();
-    		}
-    		$("#error-name").html("");
-    	} else {
-    		console.log("Nome non valido");
-    		validName = false;
-
-    		if(slideNome == 0){
-    			$("#error-name").slideDown();
-    			slideNome = 1;
-    		}
-    		
-    		$("#error-name").html("Nome non valido");
-    	}
-    	
-    	if(nome == ""){
-    		validName = false;
-    		
-    		if(slideNome == 1){
-    			slideNome = 0;
-    			$("#error-name").slideUp();
-    		}
-    		
-    		$("#error-name").html("");
-    	}
-    });
-    
-    $("#cognome").keyup(function () {
-    	let slideSurname = 0;
-    	let cognome = $("#cognome").val();
-    	
-    	if(cognome.match(nameValidator)){
-    		console.log("Cognome valido");
-    		validSurname = true;
-    		
-    		if(slideSurname == 1){
-    			slideSurname = 0;
-    			$("#error-surname").slideUp();
-    		}
-    		$("#error-surname").html("");
-    	} else {
-    		console.log("Cognome non valido");
-    		validSurname = false;
-
-    		if(slideSurname == 0){
-    			$("#error-surname").slideDown();
-    			slideSurname = 1;
-    		}
-    		
-    		$("#error-surname").html("Cognome non valido");
-    	}
-    	
-    	if(cognome == ""){
-    		validSurname = false;
-    		
-    		if(slideSurname == 1){
-    			slideSurname = 0;
-    			$("#error-surname").slideUp();
-    		}
-    		
-    		$("#error-surname").html("");
-    	}
-    });
-    
-    $("#registrati").click(function(){
-    	if(validEmail && validPwdCheck && validUsername && validName && validSurname && validPwd){
-    		$("#registrazione").submit();
-    	} else {
-    		event.preventDefault();
-    	}
-    })
-});
--- WebContent/loginPage.jsp
+++ WebContent/loginPage.jsp
@@ -106,11 +106,11 @@
                             <div class="login-reg-form-wrap">
                                 <form action="registrazione" method="post">
                                     <div class="single-input-item">
-                                        <input type="text" placeholder="Nome" id="nome" name="nome" required/>
+                                        <input type="text" placeholder="Nome" id="nome" name="nome" oninput="textValidate(this)" required/>
                                    		<p style=color:red; id="error-name"></p>
                                     </div>
                                     <div class="single-input-item">
-                                        <input type="text" placeholder="Cognome" name="cognome" id="cognome" required/>
+                                        <input type="text" placeholder="Cognome" name="cognome" id="cognome" oninput="textValidate(this)" required/>
                                         <p style=color:red; id="error-surname"></p>
                                     </div>
 									<div class="single-input-item">
@@ -118,21 +118,21 @@
                                         <p style=color:red; id="error-username"></p>
                                     </div>
                                     <div class="single-input-item">
-                                        <input type="text" placeholder="Enter your Email" name="email" id="email" required>
+                                        <input type="email" placeholder="Enter your Email" name="email" id="email" oninput="emailValidate(this)"required>
         								<p style=color:red; id="error-email"></p>
                                     </div>
 
                                     <div class="row">
                                         <div class="col-lg-6">
                                             <div class="single-input-item">
-                                                <input type="password" placeholder="Enter your Password" id="password" name="password" required/>
+                                                <input type="password" placeholder="Enter your Password" id="password" name="password" oninput="passwordValidate(this)" required/>
                                                 <p style=color:red; id="error-pwd"></p>
                                             </div>
                                         </div>
 
                                         <div class="col-lg-6">
                                             <div class="single-input-item">
-                                                <input type="password" placeholder="Repeat your Password" id="passwordCheck" name="passwordCheck" required/>
+                                                <input type="password" placeholder="Repeat your Password" id="passwordCheck" name="passwordCheck" oninput="passwordMatchValidate(document.getElementById('password').value, this.value)"required/>
                                                 <p style=color:red; id="error-pwdchk"></p>
                                             </div>
                                         </div>
